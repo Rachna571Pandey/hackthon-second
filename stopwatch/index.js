3c7855d76e0c4c743b2c9ec0e3be47bb997becd2
Rachna571Pandey <rachnapandey571@gmail.com>
2023-01-15 22:50:26 +0530

    first commit

---
+++ stopwatch/index.js
@@ -0,0 +1,70 @@
+
+let min =0;
+let sec =0;
+let count =0;
+
+
+let timer =false;
+
+function start(){
+    timer=true;
+    stopwatch();
+}
+
+
+function stop(){
+   timer=false;
+}
+
+function reset(){
+    timer=false;
+    count=0;
+    min=0;
+    sec=0;
+    document.getElementById("min").innerHTML="00";
+    document.getElementById("sec").innerHTML="00";
+    document.getElementById("count").innerHTML="00";
+}
+
+function stopwatch(){
+    if (timer ==true){
+       count =count+1;
+    
+       if(count==100){
+        sec=sec+1;
+        count=0
+       }
+
+       if(sec==60){
+        min=min+1;
+        sec=0
+       }
+   
+   
+       let minstring=min;
+       let secstring=sec;
+       let countstring=count;
+
+
+       if(min<10){
+          minstring="0"+minstring;
+
+       }
+
+       if(sec<10){
+        secstring="0"+secstring;
+
+     }
+
+     if(count<10){
+        countstring="0"+countstring;
+
+     }
+
+       document.getElementById("min").innerHTML=minstring;
+       document.getElementById("sec").innerHTML=secstring;
+       document.getElementById("count").innerHTML=countstring;
+
+        setTimeout("stopwatch()",10);
+    }
+}
